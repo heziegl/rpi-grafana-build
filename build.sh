fe79d9cdc9a37c9cd441e941e92d8f5cd0321d7e
root <git@z5r.de>
2016-08-22 10:00:04 +0000

    Grafana 3.1.1

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 
 # grafana git tag to build, or "master" to build master
 #VERSION=master-$(date +"%Y%m%d_%H%M")
-VERSION=3.0.4
+VERSION=3.1.1
 
 # host dir to copy result to
 DIST_DIR=dist-$VERSION
